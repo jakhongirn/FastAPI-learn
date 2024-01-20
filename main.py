939410cc9350c9e02c5bd6d098daef1ccc711472
Jakhongir <jahongirnagmatov@gmail.com>
2024-01-20 15:53:03 +0500

    add search method

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI
+from typing import List, Dict
+
 
 app = FastAPI(title="Learn FastAPI")
 
-RECIPES = [
+PRODUCTS: List[Dict[str, str | int]] = [
     {
         "id": 1,
-        "label": "Chicken Vesuvio",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+        "label": "Iphone 15 Pro",
+        "brand": "Apple",
+        "url": "http://www.seriouseats.com/products/2011/12/chicken-vesuvio-product.html",
     },
     {
         "id": 2,
-        "label": "Chicken Paprikash",
-        "source": "No Recipes",
-        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+        "label": "Samsung Galaxy S24",
+        "brand": "Samsung",
+        "url": "http://noproducts.com/product/chicken-paprikash/",
     },
     {
         "id": 3,
-        "label": "Cauliflower and Tofu Curry Recipe",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+        "label": "HP Pavilion Gaming",
+        "brand": "Hewlett Packard",
+        "url": "http://www.seriouseats.com/products/2011/02/cauliflower-and-tofu-curry-product.html",
     },
 ]
 
@@ -28,15 +30,34 @@ async def root():
     return {"message": "Hello world!"}
 
 
-@app.get("/recipe/{recipe_id}", status_code=200)
-async def get_recipe(*, recipe_id: int = 0) -> dict:
-    """ Get a single recipe by ID """
+@app.get("/product/{product_id}", status_code=200)
+async def get_product(*, product_id: int = 0) -> dict:
+    """ Get a single product by ID """
     
-    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    result = [product for product in PRODUCTS if product["id"] == product_id]
     
     print(result)
     if result:
         return result[0]
     else:
-        return {"message": "There is no recipes assigned with given id."}
+        return {"message": "There is no products assigned with given id."}
+
+
+@app.get("/search/", status_code=200)
+async def search_products(keyword: str | None = None, max_results: int | None = 5):
+    """Search for products based on label keyword"""
+    if not keyword: 
+        return {"results": PRODUCTS[:max_results]}
     
+    # Anoter simple solution
+    # results = []
+    # for product in PRODUCTS:
+    #     if keyword.lower() in product["label"].lower():
+    #         results.append(product)
+    # if not results:
+    #     return {"message": "There is no product with given keyword."}
+    
+    results = filter(lambda product: keyword.lower() in product["label"].lower(), PRODUCTS)
+    
+    return {"results": list(results)[:max_results]}
+
