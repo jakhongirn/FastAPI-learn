6a9e690b572cdcf5f2b66d71e1d4b0a505641fa7
Jakhongir <jahongirnagmatov@gmail.com>
2024-02-06 10:39:34 +0500

    data access layer

--- app/db/session.py
+++ app/db/session.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DB_URL = "sqlite:///example.db"
 
-engine=create_engine(
-    SQLALCHEMY_DB_URL, 
-    #required for sqlite
-    connect_args={"check_same_thread": False}
+engine = create_engine(
+    SQLALCHEMY_DB_URL,
+    # required for sqlite
+    connect_args={"check_same_thread": False},
 )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,13 +1,39 @@
 from pydantic import BaseModel, HttpUrl
+from typing import Sequence
 
-class Product(BaseModel):
-    id: int
+class ProductBase(BaseModel):
     label: str
     brand: str
     url: HttpUrl
 
-class ProductCreate(BaseModel):
+
+class ProductCreate(ProductBase):
     label: str
     brand: str
     url: HttpUrl
     submitter_id: int
+
+
+class ProductUpdate(ProductBase):
+    label: str
+
+class ProductInDBBase(ProductBase):
+    id: int
+    submitter_id: int
+
+    class Config:
+        orm_mode = True
+
+class Product(ProductInDBBase):
+    pass
+
+
+# Properties properties stored in DB
+class ProductInDB(ProductInDBBase):
+    pass
+
+
+class ProductSearchResults(BaseModel):
+    results: Sequence[Product]
+
+    
