2ad464ec643caa5f497f30fb754a4cc7a7a0b1d6
Jakhongir <jahongirnagmatov@gmail.com>
2024-02-13 21:23:14 +0500

    change api logic to interact with db

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Depends
 from product_data import PRODUCTS
 from schemas.product import Product, ProductCreate
 from typing import List, Dict
+from db.session import Session
+import crud
+import deps
 
 
 app = FastAPI(title="Learn FastAPI")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world!"}
-
+@app.get("/", status_code=200)
+async def root(
+    request: Request,
+    db: Session = Depends(deps.get_db)
+) -> dict:
+    """
+    Root GET
+    """
+    
+    products = crud.product.get_multi(db=db, limit=10)
+    return TEMPLATES.TemplateResponse("index.html", {"request": request, "products": products})
+    
+    
+    
 @app.get("/products/", status_code=200)
 async def get_all_products():
     """
@@ -20,15 +33,19 @@ async def get_all_products():
     return products
 
 @app.get("/product/{product_id}", status_code=200)
-async def get_product(*, product_id: int = 0) -> dict:
+async def get_product(
+    *, 
+    product_id: int = 0,
+    db: Session = Depends(deps.get_db)
+    ) -> dict:
     """ Get a single product by ID """
     
-    result = [product for product in PRODUCTS if product["id"] == product_id]
+    result = crud.product.get(db=db, id=product_id)
+    if not result: 
+        raise HTTPException(status_code=404, detail=f"Product with ID {product_id} not found!")
+    return result
     
-    print(result)
-    if not result:
-        raise HTTPException(status_code=404, detail=f"Product with ID {product_id} not found")
-    return result[0]
+
 
 @app.get("/search/", status_code=200)
 async def search_products(keyword: str | None = None, max_results: int | None = 5):
@@ -50,18 +67,15 @@ async def search_products(keyword: str | None = None, max_results: int | None = 
 
 
 @app.post("/product", status_code=201, response_model=Product)
-async def create_product(*, product_in: ProductCreate) -> dict:
+async def create_product(
+    *, 
+    product_in: ProductCreate,
+    db:Session = Depends(deps.get_db)
+    ) -> dict:
     """
         Create a new product (in memory only)
     """
     
-    new_prod_id = len(PRODUCTS) + 1
-    new_product = Product(
-        id=new_prod_id,
-        label=product_in.label,
-        brand=product_in.brand,
-        url=product_in.url
-    ).dict()
-    PRODUCTS.append(new_product)
+    new_product = crud.product.create(db=db, obj_in=product_in)
     
     return new_product
