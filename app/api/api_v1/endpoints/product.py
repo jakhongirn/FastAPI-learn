a431ca5c156bfc4cfd147568867611b829d1dc0f
Jakhongir <jahongirnagmatov@gmail.com>
2024-02-15 09:39:07 +0500

    add async request and sync request and middleware to compare

--- app/api/api_v1/endpoints/product.py
+++ app/api/api_v1/endpoints/product.py
@@ -5,7 +5,8 @@ from typing import List, Dict, Optional
 from sqlalchemy.orm import Session
 from app import crud
 from app import deps
-
+import httpx 
+import asyncio
 
 router = APIRouter()
 
@@ -38,7 +39,7 @@ async def get_product(
 async def search_products(
     *,
     keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
-    max_results: Optional[int] = 10,
+    max_results: Optional[int] = 100,
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
@@ -51,7 +52,7 @@ async def search_products(
     results = filter(lambda recipe: keyword.lower() in recipe.label.lower(), products)
     return {"results": list(results)[:max_results]}
 
-
+# Create product
 @router.post("/", status_code=201, response_model=Product)
 async def create_product(
     *, product_in: ProductCreate, db: Session = Depends(deps.get_db)
@@ -63,3 +64,60 @@ async def create_product(
     new_product = crud.product.create(db=db, obj_in=product_in)
 
     return new_product
+
+
+def get_reddit_top(subreddit: str, data: dict) -> None:
+    response = httpx.get(
+        f"https://www.reddit.com/r/{subreddit}/top.json?sort=top&t=day&limit=5",
+        headers={"User-agent": "recipe bot 0.1"},
+    )
+    subreddit_products = response.json()
+    subreddit_data = []
+    
+    for entry in subreddit_products["data"]["children"]:
+        score = entry["data"]["score"]
+        title = entry["data"]["title"]
+        link = entry["data"]["url"]
+        
+        subreddit_data.append(f"{str(score)}: {title} ({link})")
+    
+    data[subreddit] = subreddit_data
+    
+async def get_reddit_top_async(subreddit: str, data: dict) -> None:  # 2
+    async with httpx.AsyncClient() as client:  # 3
+        response = await client.get(  # 4
+            f"https://www.reddit.com/r/{subreddit}/top.json?sort=top&t=day&limit=5",
+            headers={"User-agent": "recipe bot 0.1"},
+        )
+
+    subreddit_recipes = response.json()
+    subreddit_data = []
+    for entry in subreddit_recipes["data"]["children"]:
+        score = entry["data"]["score"]
+        title = entry["data"]["title"]
+        link = entry["data"]["url"]
+        subreddit_data.append(f"{str(score)}: {title} ({link})")
+    data[subreddit] = subreddit_data
+
+
+@router.get("/ideas/async")
+async def fetch_ideas_async() -> dict:
+    data: dict = {}
+
+    await asyncio.gather(  # 5
+        get_reddit_top_async("recipes", data),
+        get_reddit_top_async("easyrecipes", data),
+        get_reddit_top_async("TopSecretRecipes", data),
+    )
+
+    return data
+    
+@router.get("/ideas/")
+def fetch_ideas() -> dict:
+    data: dict = {}
+    get_reddit_top("recipes", data)
+    get_reddit_top("easyrecipes", data)
+    get_reddit_top("TopSecretRecipes", data)
+
+    return data
+
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import time
 from fastapi import FastAPI, APIRouter, Query, HTTPException, Request, Depends
 from app.product_data import PRODUCTS
 from app.schemas.product import Product, ProductCreate
@@ -18,6 +19,15 @@ app = FastAPI(title="Recipe API")
 @root_router.get("/", status_code=200)
 async def root(request: Request) -> dict:
     return {"Hello world!"}
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
